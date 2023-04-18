ac89bc4acb55decbfdbdfc92fa4b863c396b92f9
Gilcimar Soares Leite <gilcimarleite@mec.gov.br>
2023-04-18 14:01:43 -0300

    Adicionado o componente forgot-password dentro do componente login.

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginComponent } from './login.component';
 
-const loginRoutes: Routes = [{ path: '', component: LoginComponent }];
+const loginRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+];
 
 @NgModule({
   declarations: [],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { NgPrimeModule } from './../shared/ng-prime/ng-prime.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ForgotPasswordComponent],
   imports: [CommonModule, LoginRoutingModule, NgPrimeModule],
 })
 export class LoginModule {}
